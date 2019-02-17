91aa1f8e370c5eb32e511a405b8cda0a51797636
Blake Barrett <blake@blakebarrett.net>
2019-02-16 22:42:24 -0800

    Adding empty project.

---
+++ Tubi TV Blake Challenge/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tubi TV Blake Challenge
+//
+//  Created by Blake Barrett on 2/16/19.
+//  Copyright © 2019 Blake Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
